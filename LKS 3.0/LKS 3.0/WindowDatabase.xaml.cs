f2dfccb1b8ac6c292c8b0df2e90febe3602dd70a
Antonisiy <meanto@yandex.ru>
2017-10-09 10:52:13 +0300

    Создаем локальную базу данных

--- LKS 3.0/LKS 3.0/WindowDatabase.xaml.cs
+++ LKS 3.0/LKS 3.0/WindowDatabase.xaml.cs
@@ -7,12 +7,16 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
+using System.Data.Entity;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Reflection;
+using System.Data.SQLite;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace LKS_3._0
 {
@@ -22,11 +26,20 @@ namespace LKS_3._0
 
     public partial class WindowDatabase : Window
     {
+        ApplicationContext dbStudent;
+        public class ApplicationContext : DbContext
+        {
+            public ApplicationContext() : base("DefaultConnection")
+            {
 
+            }
+            public DbSet<Student> Students { get; set; }
+        }
         enum ProgramMode { Admin, Student }
         public WindowDatabase()
         {
             InitializeComponent();
+
             
         }
 
@@ -47,15 +60,16 @@ namespace LKS_3._0
                 StudentsGrid.IsReadOnly = true;
             }
 
+            dbStudent = new ApplicationContext();
+
+            dbStudent.Students.Load();
+
+            StudentsGrid.ItemsSource = dbStudent.Students;
+
         }
 
-        private void StudentsGrid_Loaded(object sender, RoutedEventArgs e)
+        private void Binding_columns()
         {
-            List<Student> Data_Student = new List<Student>(3);
-            Data_Student.Add(new Student("Мещеряков","Антон","Сергеевич","3ВТИ-3ДБ-039","410","+7(985)191-84-48",Student.Student_Rank.Студент));
-            Data_Student.Add(new Student("Голвянский", "Кирилл", "Сергеевич", "3ВТИ-3ДБ-037", "410", "+7(985)222-84-48", Student.Student_Rank.Командир_взвода));
-            Data_Student.Add(new Student("Алешин", "Роман", "Анатольевич", "3ВТИ-3ДБ-039", "410", "+7(988)123-22-13", Student.Student_Rank.Журналист));
-
             Type T = typeof(Student);
             PropertyInfo[] Property_Arr = T.GetProperties();
             foreach (PropertyInfo el in Property_Arr)
@@ -63,14 +77,22 @@ namespace LKS_3._0
                 RusNameAttribute temp_attribute = (RusNameAttribute)el.GetCustomAttribute(typeof(RusNameAttribute));
 
                 DataGridTextColumn temp_column = new DataGridTextColumn();
-                    temp_column.Header = temp_attribute.Get_RussianTittle;
+                temp_column.Header = temp_attribute.Get_RussianTittle;
 
                 Binding myNewBindDef = new Binding(el.Name);
-                    temp_column.Binding = myNewBindDef;
+                temp_column.Binding = myNewBindDef;
 
                 StudentsGrid.Columns.Add(temp_column);
             }
-            StudentsGrid.ItemsSource = Data_Student;
+        }
+        private void StudentsGrid_Loaded(object sender, RoutedEventArgs e)
+        {   
+            dbStudent.Students.Add(new Student(1,"Мещеряков", "Антон", "Сергеевич", "3ВТИ-3ДБ-039", "410", "+7(985)191-84-48"));
+            dbStudent.Students.Add(new Student(2,"Голвянский", "Кирилл", "Сергеевич", "3ВТИ-3ДБ-037", "410", "+7(985)222-84-48"));
+            dbStudent.Students.Add(new Student(3,"Алешин", "Роман", "Анатольевич", "3ВТИ-3ДБ-039", "410", "+7(988)123-22-13"));
+            dbStudent.SaveChanges();
+
+            Binding_columns();    
         }
 
         private void StudentsGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -84,7 +106,7 @@ namespace LKS_3._0
             {
                 Label_FIO.Content = temp.MiddleName + " " + temp.FirstName + " " + temp.LastName;
                 Label_vzvod.Content = temp.Troop;
-                Label_rang.Content = temp.Rank;
+                //Label_rang.Content = temp.Rank;
                 Label_phone.Content = temp.MobilePhone;
             }
             else
@@ -98,7 +120,7 @@ namespace LKS_3._0
 
     }
     [System.AttributeUsage(AttributeTargets.All)] //, Inherited = false, AllowMultiple = true)
-    public partial class RusNameAttribute : Attribute
+    public class RusNameAttribute : Attribute
     {
         readonly string RussianTittle;
 
@@ -127,29 +149,33 @@ namespace LKS_3._0
         }
         public Student() // Конструктор по умолчанию
         {
+            ID = 0;
             MiddleName = "";
             FirstName = "";
             LastName = "";
             MobilePhone = "";
-            HomePhone = "";
+            //HomePhone = "";
             Group = "";
-            SpecialityName = "";
-            Rank = 0;
-            Nationality = 0;
-            Citizenship = "";
+            //SpecialityName = "";
+            //Rank = 0;
+            //Nationality = 0;
+            //Citizenship = "";
         }
 
-        public Student(string M_name, string F_name, string L_name, string group, string troop,string phone, Student_Rank rang)
+        public Student(int ID,string M_name, string F_name, string L_name, string group, string troop,string phone) //, Student_Rank rang)
         {
+            this.ID = ID;
             this.MiddleName = M_name;
             this.FirstName = F_name;
             this.LastName = L_name;
             this.Group = group;
             this.Troop = troop;
             this.MobilePhone = phone;
-            this.Rank = rang;
+            //this.Rank = rang;
         }
 
+        public int ID
+        { get; set; }
 
         [RusName("Фамилия")]
         public string MiddleName // Фамилия
@@ -164,38 +190,43 @@ namespace LKS_3._0
         [RusName("Взвод")]
         public string Troop // Взвод
         { get; set; }
-        [RusName("Телефон")]
-        public string MobilePhone // Номер моильного телефона
-        { get; set; }
 
         [RusName("Группа")]
         public string Group
         { get; set; } // Группа студента
 
-        string HomePhone // Номер домашнего телефона
-        { get; set; }
-       
-
-        // TODO добавить перечесление специальностей после уточнения(?), перечисление званий, национальностей
-
-        // возможно извенить поле на целочисленное и исспользовать перечисление
-        string SpecialityName
-        { get; set; } // Название специальности
-
-        [RusName("Звание")]
-        public Student_Rank Rank // Звание студента (перечисление)
+        [RusName("Телефон")]
+        public string MobilePhone // Номер моильного телефона
         { get; set; }
 
-        ushort Nationality // Национальность (перечисление)
-        { get; set; }
+ 
 
-        string Citizenship // Гражданство
-        { get; set; }
+        //[RusName("Звание")]
+        //public Student_Rank Rank // Звание студента (перечисление)
+        //{ get; set; }
+
+        //string HomePhone // Номер домашнего телефона
+        //{ get; set; }
+
+
+        //// TODO добавить перечесление специальностей после уточнения(?), перечисление званий, национальностей
+
+        //// возможно извенить поле на целочисленное и исспользовать перечисление
+        //string SpecialityName
+        //{ get; set; } // Название специальности
+
+
+
+        //ushort Nationality // Национальность (перечисление)
+        //{ get; set; }
+
+        //string Citizenship // Гражданство
+        //{ get; set; }
 
         //public string Добавить_поле { get; set; }
-    
 
-       
+
+
     }
 
 }
