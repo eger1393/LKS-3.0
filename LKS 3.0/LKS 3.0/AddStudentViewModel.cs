7aaf96fae26b6ef68ce809d88b0608e195788c1f
1 <1@1->
2017-11-07 22:09:00 +0300

    Делаю все в мастре =)

---
+++ LKS 3.0/LKS 3.0/AddStudentViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+namespace LKS_3._0
+{
+	class AddStudentViewModel:INotifyPropertyChanged
+	{
+		private Student adddedStudent; // Добавляемый студент
+
+
+		public event PropertyChangedEventHandler PropertyChanged;
+		public void OnPropertyChanged([CallerMemberName]string prop = "")
+		{
+			if (PropertyChanged != null)
+				PropertyChanged(this, new PropertyChangedEventArgs(prop));
+		}
+
+	}
+}
