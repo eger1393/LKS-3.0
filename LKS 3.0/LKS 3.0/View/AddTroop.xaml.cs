c0cebd2a31eea376fc5d2113038394456b46e9f6
Meanto <meanto@yandex.ru>
2018-04-27 00:00:46 +0300

    fix

--- LKS 3.0/LKS 3.0/View/AddTroop.xaml.cs
+++ LKS 3.0/LKS 3.0/View/AddTroop.xaml.cs
@@ -19,15 +19,32 @@ namespace LKS_3._0.View
     /// </summary>
     public partial class AddTroop : Window
     {
+        Troop tr_temp;
         public AddTroop(Troop temp)
         {
             InitializeComponent();
 
             DataContext = temp;
+
+            tr_temp = temp;
         }
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
+            if(checkBox_Sbori.IsChecked == true)
+            {
+                foreach (var item in tr_temp.Students)
+                {
+                    item.Status = "На сборах";
+                }
+            }
+            else
+            {
+                foreach (var item in tr_temp.Students)
+                {
+                    item.Status = "Обучается";
+                }
+            }
             DialogResult = true;
             Close();
         }
