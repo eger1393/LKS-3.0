f994df0321f23d0d1592b66420b54f71e0a64f69
eger1393 <eger1393@gmail.com>
2018-09-26 12:58:01 +0300

    Добавил пейджинг на окно отображения списка студентов.

---
+++ LKS 3.0/LKS.Web/Controllers/StudentListController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LKS.DAL.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LKS.Web.Controllers
+{
+    public class StudentListController : Controller
+    {
+		IStudentRepository studentRepository;
+		public StudentListController(IStudentRepository studentRepository)
+		{
+			this.studentRepository = studentRepository;
+		}
+		
+        public IActionResult Index()
+        {
+			var model = studentRepository.GetItems().ToList();
+            return View(model);
+        }
+
+		[HttpPost]
+		public JsonResult GetStudents([FromBody]Newtonsoft.Json.Linq.JObject data)
+		{
+			const int pageSize = 2;
+			int page;
+			if(Int32.TryParse(data.GetValue("page").ToString(),out page))
+			{
+				var students = studentRepository.GetItems().Skip((page - 1) * pageSize).Take(pageSize).ToList();
+				return Json(new { ok = true, students = students });
+			}
+			return Json(new { ok = true });
+		}
+	}
+}
