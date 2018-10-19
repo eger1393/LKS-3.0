40c898332b0e76eb1014ef78beaf257a13e4d0a7
eger1393 <eger1393@gmail.com>
2018-10-19 16:20:23 +0300

    Добавленно новое Веб приложение(начали переезд на реакт)

---
+++ LKS 3.0/LKS.Web.SPA/ClientApp/src/components/_common/studentList/index.js
@@ -0,0 +1,32 @@
+﻿import React from 'react'
+
+import { Container } from './styled'
+
+class InputTextContainer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { isFocus: false };
+
+        this.Blur = this.Blur.bind(this);
+        this.Focus = this.Focus.bind(this);
+    }
+
+    Focus(event) {
+        this.setState({ isFocus: true });
+    }
+
+    Blur(event) {
+        if (event.target.value === '')
+            this.setState({ isFocus: false });
+    }
+
+    render() {
+        return (
+            <Container>
+
+            </Container>
+        );
+    }
+}
+
+export default InputTextContainer
