cf310e0e10194843c2e2b694615cceaae3da0b83
eger1393 <eger1393@gmail.com>
2019-02-14 14:33:01 +0300

    Отрефакторил Страницу логина, убрал оттуда редакс

--- LKS 3.0/LKS.Web.SPA/ClientApp/src/redux/middleware/index.js
+++ LKS 3.0/LKS.Web.SPA/ClientApp/src/redux/middleware/index.js
@@ -1,11 +1,10 @@
 ﻿import { all, fork } from 'redux-saga/effects'
 
 import studentList from './studentList'
-import login from './login'
 import addStudent from './AddStudent'
 
 export default function rootMiddleware() {
     return function* () {
-        yield all([fork(studentList), fork(login), fork(addStudent)])
+        yield all([fork(studentList), fork(addStudent)])
     }
 }
--- LKS 3.0/LKS.Web.SPA/ClientApp/src/selectors/login.js
+++
@@ -1,5 +0,0 @@
-﻿// @flow
-import * as _ from 'lodash'
-
-export const getErrors = state => _.get(state, ['login', 'errorMessage'])
-export const getSuccessLogin = state => _.get(state, ['login','isSuccess'])
