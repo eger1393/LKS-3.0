c3431a56000eea6b4c9efc181724133da9e8dc99
eger1393 <balyko@tiburon-research.ru>
2019-08-16 15:22:23 +0300

    Починил фильтрацию студентов

--- LKS 3.0/LKS.Data/Implementation/StudentRepository.cs
+++ LKS 3.0/LKS.Data/Implementation/StudentRepository.cs
@@ -87,7 +87,7 @@ namespace LKS.Data.Implementation
                 .AsQueryable().ToList(); // todo delete tolist
             if (!string.IsNullOrEmpty(selectTroop))
                 res = res.Where(ob => ob.TroopId == selectTroop).ToList();
-            if (!filters.Any())
+            if (filters.Any())
                 FilterStudents(filters, ref res);
 
             return res.ToList();
